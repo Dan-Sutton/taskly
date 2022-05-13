cdca814d25307694449bdadfdc99ea12ba1a804a
Dan-Sutton <dewasutton@gmail.com>
2022-05-13 17:12:17 +0100

    Added hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() {
+  Hive.initFlutter("hive_boxes");
   runApp(const MyApp());
 }
 
---
+++ lib/models/task.dart
@@ -0,0 +1,11 @@
+class Task {
+  String content;
+  DateTime timestamp;
+  bool done;
+
+  Task({
+    required this.content,
+    required this.timestamp,
+    required this.done,
+  });
+}

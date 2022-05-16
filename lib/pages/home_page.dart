8cad7445258c4a5e654cc59285bfa7cd9be2ea82
Dan-Sutton <dewasutton@gmail.com>
2022-05-16 10:29:13 +0100

    More hive impletmentation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:hive_flutter/hive_flutter.dart";
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -11,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
+
   String? _newTaskContent;
   _HomePageState();
 
@@ -26,9 +28,23 @@ class _HomePageState extends State<HomePage> {
           style: TextStyle(fontSize: 25),
         ),
       ),
-      body: _tasksList(),
+      body: _tasksView(),
       floatingActionButton: _addTaskButton(),
     );
+  }
+
+  Widget _tasksView() {
+    return FutureBuilder(
+        future: Hive.openBox('tasks '),
+        builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+          if (_snapshot.connectionState == ConnectionState.done) {
+            return _tasksList();
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 
   Widget _tasksList() {
